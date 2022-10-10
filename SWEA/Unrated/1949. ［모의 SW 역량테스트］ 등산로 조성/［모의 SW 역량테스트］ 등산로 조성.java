ec0d1d615f4dcf6680a9ea8b431e2393271d3798
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-10-11 02:16:08 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 127 ms, Memory: 32,384 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,14 +3,6 @@ import java.util.*;
 
 
 public class Solution{
-    static class Pair{
-        int x, y;
-        Pair(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     static int N, K;
     static int[][] arr;
     static int[] dx = {-1,1,0,0};
@@ -18,28 +10,28 @@ public class Solution{
     static int max;
     static boolean[][] visited;
 
-    static void dfs(Pair p, int len, boolean drill){
+    static void dfs(int x, int y, int len, boolean drill){
         if(len > max) max = len;
-        visited[p.x][p.y]  = true;
+        visited[x][y]  = true;
         for(int d = 0; d < 4; d++){
-            int nx = p.x + dx[d];
-            int ny = p.y + dy[d];
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
             if(nx < 0 || ny < 0 || nx >= N || ny >= N || visited[nx][ny]) continue;
 
-            if(arr[nx][ny] < arr[p.x][p.y]) dfs(new Pair(nx, ny), len + 1, drill);
+            if(arr[nx][ny] < arr[x][y]) dfs(nx, ny, len + 1, drill);
             else{
                 if(drill) {
-                    int cut = arr[nx][ny] - (arr[p.x][p.y] - 1);    // 가능한 최소로 커팅한 수치
+                    int cut = arr[nx][ny] - (arr[x][y] - 1);    // 가능한 최소로 커팅한 수치
                     if(cut <= K){
                         arr[nx][ny] -= cut;
-                        dfs(new Pair(nx, ny), len + 1, false);
+                        dfs(nx, ny, len + 1, false);
                         arr[nx][ny] += cut;
                     }
                 }
             }
         }
-        visited[p.x][p.y] = false;
+        visited[x][y] = false;
     }
 
     public static void main(String[] args) throws Exception {
@@ -69,7 +61,7 @@ public class Solution{
 
             for(int i = 0; i < N; i++){
                 for(int j = 0; j < N; j++){
-                    if(arr[i][j] == peek) dfs(new Pair(i, j), 1, true);
+                    if(arr[i][j] == peek) dfs(i, j, 1, true);
                 }
             }
             System.out.printf("#%d %d\n", t, max);
