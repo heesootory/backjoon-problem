1abc96bc1e0baebec7cf408699d43e98f5783918
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-10-17 23:44:02 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 14852 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -21,10 +21,7 @@ public class Main{
         if(N >= 2){
             dp[2] = arr[1] + arr[2];
             for(int i = 3; i <= N; i++){
-//              dp[i] = arr[i] + Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]);
-                // 이 문제에서는 비연속적인 최댓값을 갱신하므로, 일반적인 dp배열이 무조건 최댓값을 보장하지 않음!!
                 dp[i] = Math.max(dp[i-1], arr[i] + Math.max(dp[i - 3] + arr[i - 1], dp[i - 2]));
-                // 비연속적인 직전의 값과 비교를해봐야 함!!
             }
         }
 
