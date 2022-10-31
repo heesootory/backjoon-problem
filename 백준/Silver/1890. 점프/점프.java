2ea9ce5ad5256684645aec54f5aa6d49b95f2ea2
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-11-01 04:02:47 +0900

    [Silver I] Title: 점프, Time: 132 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -5,6 +5,28 @@ public class Main{
     static int N;
     static int[][] arr;
     static long[][] dp;
+    static int[] dx = {1, 0};
+    static int[] dy = {0, 1};
+
+    static long backtrek(int x, int y){
+        if(x == N - 1 && y == N - 1){
+            return 1;
+        }
+        // 이미 저장이 되어있다면 그걸 꺼내.
+        if(dp[x][y] != -1) return dp[x][y];
+
+        // 아직 저장이 안된 dp라면.
+        dp[x][y] = 0;
+        for(int d = 0; d < 2; d++){
+            int nx = x + dx[d] * arr[x][y];
+            int ny = y + dy[d] * arr[x][y];
+            if(nx >= N || ny >= N) continue;
+
+            dp[x][y] += backtrek(nx, ny);
+        }
+        // 저장해서 꺼내죠.
+        return dp[x][y];
+    }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,23 +36,17 @@ public class Main{
         dp = new long[N][N];
         arr = new int[N][N];
 
-        for(int i = 0 ; i < N; i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < N; j++){
+            Arrays.fill(dp[i], -1);     // dp배열은 모두 -1로 채우기.
+            for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // 상향식으로 작성.
-        dp[0][0] = 1;
-        for(int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (i == N - 1 && j == N - 1) continue;
-                int jump = arr[i][j];
-                if (i + jump < N) dp[i + jump][j] += dp[i][j];
-                if (j + jump < N) dp[i][j + jump] += dp[i][j];
-            }
-        }
 
-        System.out.println(dp[N-1][N-1]);
+        backtrek(0,0);
+        
+        System.out.println(dp[0][0]);
+
     }
 }
