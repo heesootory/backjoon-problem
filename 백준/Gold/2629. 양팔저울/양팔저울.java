87d21c4d2ea43b7b972b6e7a74a73cd2e7f4fe68
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-12-14 10:55:25 +0900

    [Gold III] Title: 양팔저울, Time: 176 ms, Memory: 61180 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -28,25 +28,15 @@ public class Main {
                 if(dp[i-1][j] == 1){
                     dp[i][j] = 1;       // 이전에 이미 측정 가능한 구슬의 무게.
                     // 이미 측정 가능한 무게에서 새로운 추를 추가해서 측정이 가능한 무게들을 표시.
-                    if(j - arr[i] >= 0) dp[i][j - arr[i]] = 1;
-                    if(arr[i] - j >= 0) dp[i][arr[i] - j] = 1;
+                    dp[i][Math.abs(j - arr[i])] = 1;
                     if(j + arr[i] < len) dp[i][j + arr[i]] = 1;
                 }
                 if(j == arr[i]) dp[i][j] = 1;       // 새로운 추 하나만 가지고 측정 가능한 무게.
             }
         }
 
-//        for(int i = 0; i < N + 1; i++){
-//            for(int j = 0; j < 20; j++){
-//                System.out.print(dp[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-
         for(int i = 0; i < M; i++){
             System.out.print((dp[N][marbles[i]] == 1) ? "Y " : "N ");
         }
-
-
     }
 }
