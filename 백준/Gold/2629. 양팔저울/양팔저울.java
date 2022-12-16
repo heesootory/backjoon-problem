bef1db957ea3ac4b622bb56860371882f03fa4e4
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-12-16 22:21:33 +0900

    [Gold III] Title: 양팔저울, Time: 176 ms, Memory: 61180 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -2,46 +2,41 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, range = 15000;
-    static int[] arr;
-    static boolean[][] dp;
-    static void dfs(int idx, int weight){
-        // 이미 메모가 되어있다면 갈 필요x.
-        if(dp[idx][weight]) return;
-        dp[idx][weight] = true;
-
-        // 기저조건 - 위와의 순서가 중요! 기저가 먼저 있으면, 코드 전면 수정
-        if(idx == N) return;
-
-        // 완탐.
-        dfs(idx + 1, weight);
-        dfs(idx + 1, weight + arr[idx + 1]);
-        dfs(idx + 1, Math.abs(weight - arr[idx + 1]));
-    }
+    static int N, M;
+    static int[] arr, marbles;
+    static int[][] dp;
+    static final int len = 400001;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         N = Integer.parseInt(br.readLine());
-        arr = new int[N + 1];
+        arr = new int[N+1];
         st = new StringTokenizer(br.readLine());
-        for(int i = 1; i < N + 1; i++) arr[i] = Integer.parseInt(st.nextToken());
-
-        dp = new boolean[N + 1][range + 1];
-        dfs(0, 0);
+        for(int i = 1; i < N+1; i++) arr[i] = Integer.parseInt(st.nextToken());
 
         M = Integer.parseInt(br.readLine());
+        marbles = new int[M];
         st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-//        for(int i = 0; i < 5; i++) System.out.print(dp[N][i] + " ");
-        for(int i = 0; i < M; i++) {
-            int q = Integer.parseInt(st.nextToken());
-            if(q <= range) sb.append(dp[N][q] ? "Y " : "N ");
-            else sb.append("N ");
+        for(int i = 0; i < M; i++) marbles[i] = Integer.parseInt(st.nextToken());
+
+        dp = new int[N+1][len];
+
+        for(int i = 1; i < N + 1; i++){
+            for(int j = 0; j < len; j++){
+                if(dp[i-1][j] == 1){
+                    dp[i][j] = 1;       // 이전에 이미 측정 가능한 구슬의 무게.
+                    // 이미 측정 가능한 무게에서 새로운 추를 추가해서 측정이 가능한 무게들을 표시.
+                    dp[i][Math.abs(j - arr[i])] = 1;
+                    if(j + arr[i] < len) dp[i][j + arr[i]] = 1;
+                }
+                if(j == arr[i]) dp[i][j] = 1;       // 새로운 추 하나만 가지고 측정 가능한 무게.
+            }
         }
 
-        System.out.println(sb);
-
+        for(int i = 0; i < M; i++){
+            System.out.print((dp[N][marbles[i]] == 1) ? "Y " : "N ");
+        }
     }
 }
