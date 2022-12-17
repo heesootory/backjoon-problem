ebefba27c36c58f64ae19f08a1017af866b7063b
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2022-12-17 22:29:30 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 320 ms, Memory: 22612 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,56 +1,57 @@
 import java.io.*;
 import java.util.*;
 
-/**
- * bottom - top (상향식)
- */
 
-public class Main {
-    static int N;
+public class Main{
+    static final int row = 0;
+    static final int column = 1;
+    static final int diagonal = 2;
     static int[][] arr;
-    static long[][][] max_arr;
+    static long[][][] memo;      // 0 : 가로, 1 : 상하, 2 : 대각.
+    static int n;
+    static long DFS(int way, int x, int y){
+        // 도착 시점.
+        if(x == n-1 && y == n-1) return 1;
+        // memo 활용할 때
+        if(memo[way][x][y] != 0) return memo[way][x][y];
 
+        // memo 값이 없을 때.
+        long result = 0;     // 현재 노드에서 갈수 있는 가짓 수들의 총합!!
+
+        switch(way){
+            case row:   // 가로 -> 가로
+                if(y < n-1 && arr[x][y+1] == 0 ) result += DFS(row, x, y+1);
+                break;
+            case column:    // 세로 -> 세로
+                if(x < n-1 && arr[x+1][y] == 0) result += DFS(column, x+1, y);
+                break;
+            case diagonal:  // 대각 -> 가로, 세로
+                if(y < n-1 && arr[x][y+1] == 0 ) result += DFS(row, x, y+1);
+                if(x < n-1 && arr[x+1][y] == 0) result += DFS(column, x+1, y);
+                break;
+        }
+        // 가로, 세로, 대각 -> 대각.
+        if(x < n-1 && y < n-1 && arr[x+1][y+1] == 0 && arr[x+1][y] == 0 && arr[x][y+1] == 0) result += DFS(diagonal, x+1, y+1);
+
+        memo[way][x][y] = result;
+        return result;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N+1][N+1];
-        max_arr = new long[3][N+1][N+1];
+        n = Integer.parseInt(st.nextToken());
 
-        for(int i = 1; i < N + 1; i++){
+        arr = new int[n][n];
+        memo = new long[3][n][n];
+        for(int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
-            for(int j = 1; j < N + 1; j++){
+            for(int j = 0; j < n; j++){
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        max_arr[0][1][2] = 1;
-
-        for(int i = 1; i < N + 1; i++){
-            for(int j = 3; j < N + 1; j++){
-                if(arr[i][j] == 1) continue;
-                max_arr[0][i][j] = max_arr[0][i][j-1] + max_arr[1][i][j-1];
-                if(arr[i-1][j] != 1 && arr[i][j-1] != 1)
-                    max_arr[1][i][j] = max_arr[0][i-1][j-1] + max_arr[1][i-1][j-1] + max_arr[2][i-1][j-1];
-                max_arr[2][i][j] = max_arr[2][i-1][j] + max_arr[1][i-1][j];
-            }
-        }
-
-//        print(max_arr[0]);
-//        print(max_arr[1]);
-//        print(max_arr[2]);
-
-        System.out.println(max_arr[0][N][N] + max_arr[1][N][N] + max_arr[2][N][N]);
-
-    }
-    static void print(long[][] arr){
-        for(long[] i : arr){
-            for(long j : i) System.out.print(j + " ");
-            System.out.println();
-        }
-        System.out.println();
+        long ans = DFS(row, 0, 1);
+        System.out.println(ans);
     }
 }
-
-
