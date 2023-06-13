af6067687f6f878e3c7dc1a3b05aa93498dbd9f3
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-06-13 16:35:30 +0900

    [Gold V] Title: 줄어드는 수, Time: 124 ms, Memory: 12072 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -1,39 +1,39 @@
-import java.io.*;
+import com.sun.security.jgss.GSSUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 
 public class Main {
     static int N;
     static int[] arr = {9,8,7,6,5,4,3,2,1,0};
-    static List<Long> list  = new ArrayList<>();
+    static List<Long> list;
 
+    // 만들 수 있는 수 모조리 만들어서 list에 담기.
     static void dfs(int idx, long num){
-        // 기저조건 직전 - 리스트에 없다면 삽입.
         if(!list.contains(num)) list.add(num);
 
-        // 기저
         if(idx >= 10) return;
 
-        // 부분집합과 동일 -> 수를 고르냐 안고르냐
-        dfs(idx + 1, (num*10)+arr[idx]);      // 선택하여 더해줌.
-        dfs(idx + 1, num);          // 안선택하고 넘어감.
+        dfs(idx + 1, (num * 10) + arr[idx]);
+        dfs(idx + 1, num);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        list = new ArrayList<>();
 
-        dfs(0,0);
+        dfs(0, 0);
         Collections.sort(list);
-//        for(int i = 0; i < 30; i++){
-//            System.out.print(list.get(i) + " ");
-//        }
-        try{
-            System.out.println(list.get(N-1));
-        }catch(Exception e){
-            System.out.println(-1);
-        }
+
+        int len = list.size();
+        System.out.println((N > len) ? -1 : list.get(N-1));
+
     }
 }
