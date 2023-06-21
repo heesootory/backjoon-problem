08c746a124811b041d3275feec93f39b0d3691c3
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-06-21 18:50:31 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.57 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] arr = new int[600001];
+        
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 2;
+        arr[3] = 3;
+        
+        for(int i = 4; i <= n; i++){
+            arr[i] = (arr[i-1] + arr[i-2]) % 1000000007;
+        }
+        answer = arr[n];
+        return answer;
+    }
+}
