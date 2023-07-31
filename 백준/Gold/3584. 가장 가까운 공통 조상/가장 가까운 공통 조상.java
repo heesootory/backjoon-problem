6333e261fb2428899367628c24abb53529c4ccc1
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-07-31 16:20:43 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 252 ms, Memory: 24128 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int T, N;
+    static int Ans = -1;
+    static int[] parents;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        T = Integer.parseInt(br.readLine());
+
+        for(int t = 0; t < T; t++){
+            N = Integer.parseInt(br.readLine());
+
+            parents = new int[N + 1];
+            visited = new boolean[N + 1];
+
+            for(int n = 1; n < N; n++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                parents[b] = a;
+            }
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sun1 = Integer.parseInt(st.nextToken());
+            int sun2 = Integer.parseInt(st.nextToken());
+
+            while(sun1 != 0){
+                visited[sun1] = true;
+                sun1 = parents[sun1];
+            }
+            
+            while(true) {
+                if(visited[sun2]){
+                    Ans = sun2;
+                    break;
+                }
+
+                sun2 = parents[sun2];
+            }
+            
+            System.out.println(Ans);
+
+
+        }
+
+    }
+}
