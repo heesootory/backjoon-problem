6dff73fb0a65341c7505c8b7bd11c740ee0772fe
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-08-03 17:57:19 +0900

    [Gold II] Title: 중앙값 구하기, Time: 404 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 
 public class Main{
-
     static int T,  M;
-    static ArrayList<Integer> list;
+    static PriorityQueue<Integer> minHeap;
+    static PriorityQueue<Integer> maxHeap;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,37 +13,49 @@ public class Main{
         T = Integer.parseInt(br.readLine());
 
         for(int t = 0; t < T; t++){
+            minHeap = new PriorityQueue<>();
+            maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
             M = Integer.parseInt(br.readLine());
-            list = new ArrayList<>();
             System.out.println(M/2 + 1);
-            int m = 0;
+            int m = 1;
+            boolean flag = false;
             while(true){
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                boolean b = false;
                 for(int i = 0; i < 10; i++){
-                    list.add(Integer.parseInt(st.nextToken()));
-                    m++;
-                    if(m % 2 != 0){
-                        Collections.sort(list);
-                        System.out.print(list.get(m/2) +  " ");
+                    int x = Integer.parseInt(st.nextToken());
+
+                    // 입력받는 값을 넣어주는 방법. - 3개까지는 그냥 minHeap에 넣기
+                    if (m > 3) {
+                        if(x > minHeap.peek()) minHeap.add(x);
+                        else if(x < maxHeap.peek()) maxHeap.add(x);
+                        else minHeap.add(x);
+                    }
+                    else{
+                        minHeap.add(x);
                     }
 
-                    if(m == M) {
-                        b = true; break;
+                    // 홀수 번째인 경우
+                    if(m % 2 != 0){
+                        while(minHeap.size() - 1 > maxHeap.size()){
+                            maxHeap.add(minHeap.poll());
+                        }
+                        while(minHeap.size() < maxHeap.size()){
+                            minHeap.add(maxHeap.poll());
+                        }
+                        System.out.print(minHeap.peek() + " ");
+                    }
+
+                    m++;
+                    if(m == M + 1) {
+                        flag = true; break;
                     }
                 }
-                if(b) break;
+                if(flag) break;
             }
 
 //            for(int i : list) System.out.print(i + " ");
             System.out.println();
 
-
-
-
-
         }
-
-
     }
 }
