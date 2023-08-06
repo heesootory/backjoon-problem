3d46c5b816e2403dcc09d13c7a561c840f87af16
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-08-06 20:32:54 +0900

    [Bronze II] Title: Hashing, Time: 132 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    static int L;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        L = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        int ans = 0;
+        int mod = 1234567891;
+        for(int i = 0; i < L; i++){
+            int a = str.charAt(i) - 'a' + 1;
+            int r = (int)((long)Math.pow(31, i) % mod);
+            int ar = a * r % mod;
+            ans = (int)((ans + ar) % mod);
+        }
+
+        System.out.println(ans);
+
+    }
+}
