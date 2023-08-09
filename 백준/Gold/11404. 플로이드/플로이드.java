19dee7255ee8faf15231d55bdb43c8d7852b8e58
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-08-09 12:52:50 +0900

    [Gold IV] Title: 플로이드, Time: 596 ms, Memory: 45024 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,50 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-
-    static int n, m;
+public class Main{
+    static int N, M;
     static int[][] dp;
-    static final int INF = 123456789;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        dp = new int[n+1][n+1];
-        for(int i = 1; i < n + 1; i++) {
-            for(int j = 1; j < n + 1; j++){
-                if(i != j) dp[i][j] = INF;
-            }
-        }
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        dp = new int[N + 1][N + 1];
 
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            if(cost < dp[start][end]) dp[start][end] = cost;
+
+            dp[from][to] = (dp[from][to] == 0) ? cost : Math.min(dp[from][to], cost);
         }
 
-        // 플로이드-워셜
-        for(int k = 1; k < n + 1; k++){     // 경
-            for(int i = 1; i < n + 1; i++){     // 출
-                for(int j = 1; j < n + 1; j++){     // 도
-                    dp[i][j] = Math.min(dp[i][k] + dp[k][j], dp[i][j]);
+        for(int k = 1; k < N + 1; k++){
+            for(int s = 1; s < N + 1; s++){
+                for(int e = 1; e < N + 1; e++){
+                    if(s == e) continue;
+                    if(dp[s][k] != 0 && dp[k][e] != 0){
+                        int sum = dp[s][k] + dp[k][e];
+                        dp[s][e] = (dp[s][e] == 0) ? sum : Math.min(dp[s][e], sum);
+                    }
                 }
             }
         }
 
-        for(int i = 1; i < n+1; i++){
-            for(int j = 1; j < n+1; j++){
-                if(dp[i][j] == INF) dp[i][j] = 0;
+        for(int i = 1; i < N + 1; i++){
+            for(int j = 1; j < N + 1; j++){
                 System.out.print(dp[i][j] + " ");
             }
             System.out.println();
         }
-
 
     }
 }
