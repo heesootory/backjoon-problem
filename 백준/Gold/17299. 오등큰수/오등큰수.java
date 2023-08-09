1426ea4cef9976341d9d8005f6e3e0887a1d6fc3
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-08-09 22:30:44 +0900

    [Gold III] Title: 오등큰수, Time: 1012 ms, Memory: 141568 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -1,49 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    static int n;
-    static Map<Integer, Integer> map;
-    static int[] arr;
-    static int[] ansArr;
-    static Stack<Integer> stack;
 
+public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        map = new HashMap<>();
-        stack = new Stack<>();
-        arr = new int[n];
-        ansArr = new int[n];
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] ans = new int[N];
+        int[] cntArr = new int[1000001];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0 ; i < n; i++){
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
-            if(map.containsKey(num)) map.put(num, map.get(num) + 1);
-            else map.put(num, 1);
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            cntArr[arr[i]]++;
         }
 
-        for(int i = 0; i < n; i++){
-            if(stack.isEmpty()) stack.push(i);
-            else{
-                while(map.get(arr[stack.peek()]) < map.get(arr[i])){
-                    ansArr[stack.pop()] = arr[i];
-                    if(stack.isEmpty()) break;
-                }
+        Stack<Integer> stack = new Stack<>();
 
-                stack.push(i);
+        for(int i = 0; i < N; i++){
+            while(!stack.isEmpty() && cntArr[arr[stack.peek()]] < cntArr[arr[i]]){
+                int idx = stack.pop();
+                ans[idx] = arr[i];
             }
+
+            stack.push(i);
         }
 
-        while(!stack.isEmpty()) ansArr[stack.pop()] = -1;
+        while(!stack.isEmpty()){
+            int idx = stack.pop();
+            ans[idx] = -1;
+        }
+
         StringBuilder sb = new StringBuilder();
-        for(int i : ansArr) sb.append(i).append(" ");
+        for(int i : ans) sb.append(i).append(" ");
         System.out.println(sb);
-
-
 
     }
 }
