6bfef07f334e9b5dca7dc9439fb9ea708a3ace81
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-10-22 13:52:39 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 25.07 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -3,25 +3,14 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[2];
         
-        int binary_change_cnt = 0;
-        int zero_remove_cnt = 0;
-        
         while( !s.equals("1")){
-            
-            int one_cnt = 0;
-            
-            for(int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if(c == '0') zero_remove_cnt++;
-                else one_cnt++;
-            }
-            
-            s = Integer.toBinaryString(one_cnt);
-            binary_change_cnt++;
+            int len = s.length();
+            s = s.replaceAll("0", "");
+            int tmp = s.length();
+            answer[1] += (len - tmp);
+            s = Integer.toBinaryString(tmp);
+            answer[0]++;
         }
-        
-        answer[0] = binary_change_cnt;
-        answer[1] = zero_remove_cnt;
         
         return answer;
     }
