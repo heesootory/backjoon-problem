ae40bbae267745af13b4486a2fbd92fd346ea1af
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-10-24 14:57:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.17 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,14 @@
+class Solution {
+    static final int INF = 1234567;
+    
+    public long solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % INF;
+        }
+        
+        return dp[n];
+    }
+}
