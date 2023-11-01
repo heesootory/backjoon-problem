3a5ea0ee179e5d58e0ad0fba410f76a36dd27e45
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-11-01 18:47:04 +0900

    [level 2] Title: 귤 고르기, Time: 49.79 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,38 +1,22 @@
 import java.util.*;
 
-class Pair implements Comparable<Pair>{
-    int num;
-    int cnt;
-    Pair(int num, int cnt){
-        this.num = num;
-        this.cnt = cnt;
-    }
-    
-    @Override
-    public int compareTo(Pair o){
-        return o.cnt - this.cnt;
-    }
-}
-
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
         
-        ArrayList<Pair> list = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
         
+        // map에 귤 종류 - 갯수 저장
         for(int i : tangerine) map.put(i, map.getOrDefault(i, 0) + 1);
         
-        for(int i : map.keySet()){
-            list.add(new Pair(i, map.get(i)));
-        }
-        
-        Collections.sort(list);
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> map.get(o2) - map.get(o1));
         
         int sum = 0;
-        for(Pair p : list){
+        for(int i : list){
             if(sum >= k) break;
-            sum += p.cnt;
+            
+            sum += map.get(i);
             answer++;
         }
                 
