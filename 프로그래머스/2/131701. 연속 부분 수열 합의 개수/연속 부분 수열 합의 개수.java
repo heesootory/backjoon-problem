5a04eb6264bd090a5f9edde36140553beb161126
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-11-02 13:56:13 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 90.51 ms, Memory: 132 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -4,14 +4,12 @@ class Solution {
     public int solution(int[] elements) {
         int len = elements.length;
         Set<Integer> set = new HashSet<>();
+        int[] dp = new int[len];
         
         for(int i = 1; i <= len; i++){          // 부분 수열의 개수.
             for(int j = 0; j < len; j++){       // 수열 카운트를 시작할 인덱스
-                int sum = 0;
-                for(int cnt = 0, idx = j; cnt < i; cnt++, idx++){
-                    sum += elements[idx % len];
-                }
-                set.add(sum);
+                dp[j] += elements[(j + i) % len];
+                set.add(dp[j]);
             }
         }
         
