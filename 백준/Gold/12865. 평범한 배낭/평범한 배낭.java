87f863a5621b6af34238643f87387f578a0ebfeb
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2023-12-12 17:43:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 164 ms, Memory: 51296 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 
-public class Main {
+public class Main{
     static int N, K;
+    static int[] weights;
+    static int[] values;
     static int[][] dp;
-    static int[] w, v;
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        w = new int[N+1];
-        v = new int[N+1];
+        weights = new int[N + 1];
+        values = new int[N + 1];
 
         for(int i = 1; i < N + 1; i++){
             st = new StringTokenizer(br.readLine());
-            w[i] = Integer.parseInt(st.nextToken());
-            v[i] = Integer.parseInt(st.nextToken());
+
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            weights[i] = weight;
+            values[i] = value;
         }
 
-        dp = new int[N+1][K+1];
+        dp = new int[N + 1][K + 1];
+        for(int i = 1; i < N + 1; i++) Arrays.fill(dp[i], -1);
 
         for(int i = 1; i < N + 1; i++){
-            for(int j = 1; j < K + 1; j++){
-                dp[i][j] = dp[i-1][j];
-                if(j - w[i] >= 0) dp[i][j] = Math.max(dp[i-1][j-w[i]] + v[i] , dp[i][j]);
+            for(int j = 0; j < K + 1; j++){
+                // 물건을 넣을 수 없을 때
+                if(weights[i] > j) dp[i][j] = dp[i - 1][j];
+                // 물건을 넣을 수 있을 떄
+                else{
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i]] + values[i]);
+                }
             }
         }
 
         System.out.println(dp[N][K]);
+
 
     }
 
