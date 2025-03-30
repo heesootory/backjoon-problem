a365fb8c225c357f1ac2045a69e6de220ec13b7b
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2025-03-30 22:47:57 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 19092 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -3,30 +3,31 @@ import java.util.*;
 
 
 public class Main {
-
+    static String str1, str2;
     static int[][] dp;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        String str1 = br.readLine();
-        String str2 = br.readLine();
-        int len1 = str1.length();
-        int len2 = str2.length();
+        str1 = br.readLine();
+        str2 = br.readLine();
 
-        dp = new int[len1+1][len2+1];
+        dp = new int[str1.length() + 1][str2.length() + 1];
 
-        for(int i = 1; i <= len1; i++){
-            for(int j = 1; j <= len2; j++){
-                if(str1.charAt(i-1) == str2.charAt(j-1)){
+        for(int i = 1; i < str1.length() + 1; i++){
+            for(int j = 1; j < str2.length() + 1; j++){
+                int idx1 = i - 1;
+                int idx2 = j - 1;
+
+                if(str1.charAt(idx1) == str2.charAt(idx2)){
                     dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
             }
         }
 
-        System.out.println(dp[len1][len2]);
+        System.out.println(dp[str1.length()][str2.length()]);
 
     }
+
 }
