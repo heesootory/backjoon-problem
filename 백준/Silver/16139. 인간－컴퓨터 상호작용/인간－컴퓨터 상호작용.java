383092124894e77176c3a1c489a594f37a2102d6
Heesoo Choi <102863300+heesootory@users.noreply.github.com>
2025-03-31 17:09:57 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 680 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -7,6 +7,7 @@ public class Main {
     static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         S = br.readLine();
         dp = new int[26][S.length()];
@@ -27,9 +28,11 @@ public class Main {
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            if(start == 0) System.out.println(dp[idx][end]);
-            else System.out.println(dp[idx][end] - dp[idx][start - 1]);
+            if(start == 0) sb.append(dp[idx][end]).append("\n");
+            else sb.append(dp[idx][end] - dp[idx][start - 1]).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
 
